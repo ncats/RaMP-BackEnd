59b1c61a3a62419dbd159b1ef4694df0cbb0829e
bofeeee <8196385@qq.com>
2018-01-18 14:19:48 -0500

    RaMP fixer

--- src/update/RaMPFixer.py
+++ src/update/RaMPFixer.py
@@ -1,11 +1,33 @@
 from update.RaMPDatabase import RaMPDatabase
 
 class RaMPFixer(RaMPDatabase):
-    
+    '''
+    This class simulates the function of the C# code in RaMP
+    Get the original RaMP data from MySQL, then do the following things:
+    1) For each table, find the missing data cell, and remove the entire row since the map
+    is not successful. (Some columns are deliberated to left empty with 'NA'.)
+    2) Remove special character such as ' ', and wrong data in the cell. Consider drop entire
+    row based on conditions
+    3) Remapping corrected RaMP ID relations based on corrected data.
+    4) Create and import new RaMP data to the database.
+    '''
     
     def __init__(self):
         super().__init__()
         
+        
     
-    def hello_world(self):
+    def create_new_db(self):
+        conn = self.connectToRaMP(dbname = None)
+        
+        with conn.cursor() as cur:
+            query = "create database mathelabramp2;"
+            cur.execute(query)
+                    
+            
+        conn.commit()
+            
+        
+        conn.close()
+        
         print("hi")
