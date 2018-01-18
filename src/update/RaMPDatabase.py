636573f5bee9b276621f5fb34a879b949c1aea55
bofeeee <8196385@qq.com>
2018-01-18 14:19:48 -0500

    RaMP fixer

--- src/update/RaMPDatabase.py
+++ src/update/RaMPDatabase.py
@@ -7,9 +7,12 @@ class RaMPDatabase():
     '''
     This class is the super class of all checker, updater class 
     It contains general functions to aid genral functionality of other classes
+    
+    attribute str dbname database name for the mysql
+     
     '''
     def __init__(self):
-        self.name = "Hello World"
+        
         self.tables = ["analyte",
                        "analytehasontology",
                        "analytehaspathway",
@@ -20,11 +23,12 @@ class RaMPDatabase():
                        "source"]
         
         
+        
     def check_path(self,dir):
         '''
         This fucntion check if this directory exists, otherwise it will create one
-        - dir the directory to check or created.
-        - return True if the path has been created successfully
+        - param dict dir: The directory to check or created.
+        - return: True if the path has been created successfully
         '''
         if not os.path.exists(dir):
             try:
@@ -35,13 +39,31 @@ class RaMPDatabase():
                     raise
         
     
-    def connectToRaMP(self):
-        conn = pymysql.connect(host = "localhost",user="root",
-                               password = "Ehe131224",
-                               db = "mathelabramp",
-                               charset = "utf8mb4",
-                               cursorclass = pymysql.cursors.DictCursor)
-        
+    def connectToRaMP(self, host= "localhost", user = "root"
+                      ,password = "Ehe131224",dbname = "mathelabramp"):
+        '''
+        Connect to local RaMP database by MySQL
+        - param str host host name for the mysql connection
+        - param str user username for the mysql conncection 
+        - param str dbname database name for connection if None: connect to the database page
+        instead of table page
+        - param str password the password you used for you computer's mysql database 
+        '''
+        if dbname is not None:
+            conn = pymysql.connect(host = host,
+                                   user= user,
+                                   password = password,
+                                   db = dbname,
+                                   charset = "utf8mb4",
+                                   cursorclass = pymysql.cursors.DictCursor)
+        else:
+            conn = pymysql.connect(host = host,
+                                   user= user,
+                                   password = password,
+                                   charset = "utf8mb4",
+                                   cursorclass = pymysql.cursors.DictCursor)
+        return conn
+    '''
         try: 
             with conn.cursor() as cur:
                 df = pd.read_sql("select * from source;",conn)
@@ -50,3 +72,4 @@ class RaMPDatabase():
         
         finally:
             conn.close()
+            '''
--- tests/RaMPDatabaseTestCase.py
+++ tests/RaMPDatabaseTestCase.py
@@ -5,13 +5,10 @@ import timeit
 class RaMPDatabaseTest(unittest.TestCase):
     def testMain(self):
         ramp = RaMPDatabase()
-        df = ramp.connectToRaMP()
+        
         fixer = RaMPFixer()
-        fixer.hello_world()
-        print(df)
-        print(df.loc[13,"commonName"] == "")
-        print(type(df.loc[13,"commonName"]))
-        print(len(df))
+        fixer.create_new_db()
+        
         
         
 
