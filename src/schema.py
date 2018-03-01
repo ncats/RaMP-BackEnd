d0f528ba6b5c41bd7201dfee67ebc69400fdae0c
bofeeee <8196385@qq.com>
2018-03-01 15:14:16 -0500

    prepend IDtype to SourceID

--- src/schema.py
+++ src/schema.py
@@ -10,98 +10,108 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float, Boolean,Forei
 This script create a RaMP database with all tables based on the schema
 
 '''
-
-engine = sqla.create_engine('mysql+pymysql://root:Ehe131224@localhost/updateRaMP')
+class RaMP_schema():
+    # define all parameters for the database.
+    password = 'Ehe131224'
+    username = 'root'
+    host = 'localhost'
+    dbname = 'updateRaMP'
+    engine = sqla.create_engine('mysql+pymysql://{}:{}@{}/{}'\
+                                         .format(username,password,host,dbname))
+        
+    if not database_exists(engine.url):
+        create_database(engine.url)
+        
     
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    Base = declarative_base()
+    Base.metadata.create_all(engine)
 
-if not database_exists(engine.url):
-    create_database(engine.url)
+
+    class Analyte(Base):
+        __tablename__ = 'analyte'
+        rampId = Column(String(30),primary_key = True)
+        type = Column(String(30))
+        sources = relationship('Source',back_populates = 'analyte')
+        
+        def __repr__(self):
+            return 'Analyte {}: Type {}'.format(self.rampId,self.type)
     
-
-Session = sessionmaker(bind = engine)
-session = Session()
-Base = declarative_base()
-
-
-class Analyte(Base):
-    __tablename__ = 'analyte'
-    rampId = Column(String(30),primary_key = True)
-    type = Column(String(30))
-    sources = relationship('Source',back_populates = 'analyte')
+    class Analytehasontology(Base):
+        __tablename__ = 'analytehasontology'
+        rampCompoundId = Column(String(30),primary_key = True)
+        rampOntologyIdLocation = Column(String(30),primary_key = True)
+        def __repr__(self):
+            return 'Analyte {}: Ontology: {}'.format(self.rampCompoundId,self.rampOntologyIdLocation)
     
-    def __repr__(self):
-        return 'Analyte {}: Type {}'.format(self.rampId,self.type)
-
-class Analytehasontology(Base):
-    __tablename__ = 'analytehasontology'
-    rampCompoundId = Column(String(30),primary_key = True)
-    rampOntologyIdLocation = Column(String(30),primary_key = True)
-    def __repr__(self):
-        return 'Analyte {}: Ontology: {}'.format(self.rampCompoundId,self.rampOntologyIdLocation)
-
-class Analytehaspathway(Base):
-    __tablename__ = 'analytehaspathway'
-    rampId = Column(String(30),primary_key = True)
-    pathwayRampId = Column(String(30),primary_key = True)
-    pathwaySource = Column(String(30))
-    def __repr__(self):
-        return 'Analyte {}: Pathway {}: PathwaySource {}'.format(self.rampId,
-                                                                 self.pathwayRampId,
-                                                                 self.pathwaySource)
-class Analytesynonym(Base):
-    __tablename__ = 'analytesynonym'
-    Synonym = Column(String(500),primary_key = True)
-    rampId = Column(String(30),ForeignKey('analyte.rampId'))
-    geneOrCompound = Column(String(30))
-    source = Column(String(30))
-    def __repr__(self):
-        return 'Synonym {}: rampId {}: geneOrCompound {}: source {}'\
-            .format(self.Synonym,self.rampId,self.geneOrCompound,self.source)
-
-class Catalyzed(Base):
-    __tablename__ = 'catalyzed'
-    rampCompoundId = Column(String(30),primary_key = True)
-    rampGeneId = Column(String(30),primary_key = True)
+    class Analytehaspathway(Base):
+        __tablename__ = 'analytehaspathway'
+        rampId = Column(String(30),primary_key = True)
+        pathwayRampId = Column(String(30),primary_key = True)
+        pathwaySource = Column(String(30))
+        def __repr__(self):
+            return 'Analyte {}: Pathway {}: PathwaySource {}'.format(self.rampId,
+                                                                     self.pathwayRampId,
+                                                                     self.pathwaySource)
+    class Analytesynonym(Base):
+        __tablename__ = 'analytesynonym'
+        Synonym = Column(String(500),primary_key = True)
+        rampId = Column(String(30),ForeignKey('analyte.rampId'))
+        geneOrCompound = Column(String(30))
+        source = Column(String(30))
+        def __repr__(self):
+            return 'Synonym {}: rampId {}: geneOrCompound {}: source {}'\
+                .format(self.Synonym,self.rampId,self.geneOrCompound,self.source)
     
-    def __repr__(self):
-        return 'Compound {}: Gene {}'\
-            .format(self.rampCompoundId,self.rampGeneId)
+    class Catalyzed(Base):
+        __tablename__ = 'catalyzed'
+        rampCompoundId = Column(String(30),primary_key = True)
+        rampGeneId = Column(String(30),primary_key = True)
+        
+        def __repr__(self):
+            return 'Compound {}: Gene {}'\
+                .format(self.rampCompoundId,self.rampGeneId)
+    
+    class Ontology(Base):
+        __tablename__ = 'ontology'
+        rampOntologyIdLocation = Column(String(30),primary_key = True)
+        commonName = Column(String(30))
+        biofluidORcellular = Column(String(30))
+        def __repr__(self):
+            return 'Ontology {}: CommonName {}: Type {}'\
+                .format(self.rampOntologyIdLocation,
+                        self.commonName,
+                        self.biofluidORcellular)
+    class Pathway(Base):
+        __tablename__ = 'pathway'
+        pathwayRampId = Column(String(30))
+        sourceId =Column(String(30),primary_key = True)
+        type = Column(String(30))
+        pathwayCategory = Column(String(30))
+        pathwayName = Column(String(30))
+        
+        def __repr__(self):
+            return 'Pathway {}: Source Id {}: Type {}: Category {}: Name {}'\
+                .format(self.pathwayRampId,self.sourceId,
+                        self.type,self.pathwayCategory,self.pathwayName)
+    
+    class Source(Base):
+        __tablename__ = 'source'
+        sourceId = Column(String(30),primary_key = True)
+        rampId = Column(String(30),ForeignKey('analyte.rampId'))
+        IDtype = Column(String(30))
+        geneOrCompound = Column(String(30))
+        commonName = Column(String(30))
+        analyte = relationship('Analyte',back_populates = 'sources')
+        def __repr__(self):
+            return '|SourceId {}: RampId {}: IDtype {}: geneOrCompound {}: commonName {}|'\
+                .format(self.sourceId,self.rampId,self.IDtype,self.geneOrCompound,
+                        self.commonName)
+            
+            
+        
 
-class Ontology(Base):
-    __tablename__ = 'ontology'
-    rampOntologyIdLocation = Column(String(30),primary_key = True)
-    commonName = Column(String(30))
-    biofluidORcellular = Column(String(30))
-    def __repr__(self):
-        return 'Ontology {}: CommonName {}: Type {}'\
-            .format(self.rampOntologyIdLocation,
-                    self.commonName,
-                    self.biofluidORcellular)
-class Pathway(Base):
-    __tablename__ = 'pathway'
-    pathwayRampId = Column(String(30))
-    sourceId =Column(String(30),primary_key = True)
-    type = Column(String(30))
-    pathwayCategory = Column(String(30))
-    pathwayName = Column(String(30))
-    
-    def __repr__(self):
-        return 'Pathway {}: Source Id {}: Type {}: Category {}: Name {}'\
-            .format(self.pathwayRampId,self.sourceId,
-                    self.type,self.pathwayCategory,self.pathwayName)
 
-class Source(Base):
-    __tablename__ = 'source'
-    sourceId = Column(String(30),primary_key = True)
-    rampId = Column(String(30),ForeignKey('analyte.rampId'))
-    IDtype = Column(String(30))
-    geneOrCompound = Column(String(30))
-    commonName = Column(String(30))
-    analyte = relationship('Analyte',back_populates = 'sources')
-    def __repr__(self):
-        return 'SourceId {}: RampId {}: IDtype {}: geneOrCompound {}: commonName {}'\
-            .format(self.sourceId,self.rampId,self.IDtype,self.geneOrCompound,
-                    self.commonName)
     
-    
-Base.metadata.create_all(engine)
+        
--- src/update/RaMPDatabase.py
+++ src/update/RaMPDatabase.py
@@ -13,7 +13,8 @@ class RaMPDatabase():
     '''
     def __init__(self):
         
-        self.table_names = ["analyte",
+        self.table_names = [
+                       "analyte",
                        "analytehasontology",
                        "analytehaspathway",
                        "analytesynonym",
@@ -62,66 +63,5 @@ class RaMPDatabase():
                                    password = password,
                                    charset = "utf8mb4",
                                    cursorclass = pymysql.cursors.DictCursor)
-        return conn
-        def create_new_db(self,database):
-            '''
-            This function will create a new database on MySQL
-            if the database already existed, it will drop the previous one 
-            '''
-            conn = self.connectToRaMP(dbname = None)
-            
-            with conn.cursor() as cur:
-                query = "create database "+database + ";"
-                try:
-                    cur.execute(query)
-                    conn.commit()
-                except pymysql.err.ProgrammingError:
-                    query2 = "drop database mathelabramp2;"
-                    cur.execute(query2)
-                    conn.commit()
-                    cur.execute(query)
-                    conn.commit()  
-                    
-                finally:
-                    conn.close()           
+        return conn     
         
-    def create_tbs(self,database):
-        '''
-        This function create tables based on the super class table_name attributes
-        This part of sql code is exactly same as the C# part. If the table already existes,
-        the table will be dropped.
-        param str database the database name we want to connect
-        '''        
-        conn = self.connectToRaMP(dbname = "mathelabramp2")
-        
-        
-        CreateTable = ["create table source(sourceId VARCHAR(30), rampId VARCHAR(30), IDtype VARCHAR(30), geneOrCompound VARCHAR(30),commonName VARCHAR(30), PRIMARY KEY (sourceId)) engine = InnoDB;",
-                       "create table analyte (rampId VARCHAR(30), type VARCHAR(30), PRIMARY KEY (rampId)) engine = InnoDB;",
-                       "create table pathway (pathwayRampId VARCHAR(30), sourceId VARCHAR(30),type VARCHAR(30), pathwayCategory VARCHAR(30), pathwayName VARCHAR(250), PRIMARY KEY (pathwayRampId)) engine = InnoDB;",
-                       "create table analyteHasPathway (rampId VARCHAR(30), pathwayRampId VARCHAR(30), type VARCHAR(30)) engine = InnoDB;",
-                       "create table analyteSynonym (Synonym VARCHAR(500), rampId VARCHAR(30), geneOrCompound VARCHAR(30), source VARCHAR(30)) engine = InnoDB;",
-                       "create table catalyzed (rampCompoundId VARCHAR(30), rampGeneId VARCHAR(30)) engine = InnoDB;",
-                       "create table analyteHasOntology (rampCompoundId VARCHAR(30), rampOntologyIdLocation VARCHAR(30)) engine = InnoDB;",
-                       "create table ontology (rampOntologyIdLocation VARCHAR(30), commonName VARCHAR(30), biofluidORcellular VARCHAR(30)) engine = InnoDB;"]
-        with conn.cursor() as cur:
-            for sql in CreateTable:
-                cur.execute(sql)
-                conn.commit()
-        
-        conn.close()
-        
-    def drop_database(self,database = None):
-        '''
-        Drop the given database, call this function before you want to create new database
-        param str database the database name you want to drop
-        '''
-        assert database != None, "No input! You should provide database name"
-        assert type(database) is str, "The database name should be string"
-        conn = self.connectToRaMP(dbname = None)
-        with conn.cursor() as cur:
-            sql = "drop database " + database +";"
-            try:
-                cur.execute(sql)
-                conn.commit()
-            finally:
-                conn.close()
