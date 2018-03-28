717af9ebbc37fce57283ae97449e712c47632b82
mathe Lab <mathelab@Liz-MacBook-Pro.local>
2018-03-28 17:12:27 -0400

    update database contined

--- main/updateWikiPathways.py
+++ main/updateWikiPathways.py
@@ -1,7 +1,25 @@
 from updateSQL import RampUpdater
 import pickle as pk
-if __name__ == '__main__':
+def getUpdateObjectPkl():
     pkf = open('../misc/output/wikipathwayRdfPk.pkl','rb')
     wp = pk.load(pkf)
+    print(wp.pathwayWithMetabolitesDictionary)
+    pkf.close()
     rp = RampUpdater(wp)
-    rp.checkNewEntry()
+    rp.checkNewAnalyteEntry('compound')
+    rp.checkNewPathwayEntry()
+    pkf2 = open('../misc/output/updateObject328.pkl','wb')
+    pk.dump(rp,pkf2,pk.HIGHEST_PROTOCOL)
+    pkf2.close()
+    print('New pathway: {}'.format(len(rp.newRampPathway)))
+    
+def updatingRamp():
+    pkf = open('../misc/output/updateObject328.pkl','rb')
+    rp = pk.load(pkf)
+    pkf.close()
+    rp2 = RampUpdater(rp)
+    rp2.newRampCompound = rp.newRampCompound
+    rp2.newRampPathway = rp.newRampPathway
+    rp2.writeToRamp('wiki')
+if __name__ == '__main__':
+    updatingRamp()
