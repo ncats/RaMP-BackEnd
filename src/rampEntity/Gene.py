f53a09c01ade1deda2cfe89b44a7470e37793350
johnbraisted <jb212828@gmail.com>
2020-12-01 09:15:43 -0500

    Incr. updates to gene for gene->path and entitiy builder

--- src/rampEntity/Gene.py
+++ src/rampEntity/Gene.py
@@ -10,8 +10,7 @@ class Gene(object):
     '''
     classdocs
     '''
-
-
+    
     def __init__(self):
         '''
         Constructor
@@ -29,7 +28,7 @@ class Gene(object):
         # keys are source, values are dictiontaries of source id to commonName
         self.commonNameDict = dict()
         
-        self.synonymDict = dict()
+        self.synonymList = list()
                 
         self.primarySource = ""
 
@@ -65,6 +64,61 @@ class Gene(object):
         # maintain as a unique list
         if sourceName not in self.sources:
             self.sources.append(sourceName)
+            
+    def addCommonName(self, id, commonName, source):
+        if source not in self.commonNameDict:            
+            self.commonNameDict[source] = dict()
+        self.commonNameDict[source][id] = commonName
+        self.addSynonym(commonName)
+        
+    def addSynonym(self, synonym):
+        if synonym not in self.synonymList:
+            self.synonymList.append(synonym)
+            
+    def addPathway(self, pathway):
+        if pathway not in self.pathways:
+            self.pathways.append(pathway)        
+        
+    def printGene(self):
+        s = "rampId: " + self.rampId + "\n"        
+        for source in self.idDict:
+            for id in self.idDict[source]:
+                s = s + "id: " + id + ", source: " + source + "\n"
+        for source in self.commonNameDict:
+            for id in self.commonNameDict[source].keys():
+                s = s + "id: " + id + ", source: " + source + ", name:" + self.commonNameDict[source][id]+ "\n"
+        for syn in self.synonymList:
+            s = s + "syn: " + syn + "\n"
+        
+        print(s)
+        
+        print("pathway count: " + str(len(self.pathways)))
+        
+        for pathway in self.pathways:
+            pathway.printPathway()
+            
+             
+    
+    
+    '''
+    The original code in writeToSQL would steal the commonName for a gene
+    from another common name when one instance of the gene (from the same source) lacked a common name.
+    It's a bit of a hack but for genes it will complete this field for the source table.
+    Also, all common names will have distinct rows with the same rampId.
+    This means that common name will resolve to a ramp id from any common name available.
+    '''
+    def resolveCommonNames(self):
+        for source in self.idDict:
+            for id in self.idDict[source]:
+                if source in self.commonNameDict and id not in self.commonNameDict[source]:
+                    # now we know we have a common name dictionary for the source
+                    # and our id doesn't have a commmon name entry.
+                    
+                    #grab a key
+                    keyId = list(self.commonNameDict[source].keys())[0]
+                    self.commonNameDict[source][id] = self.commonNameDict[source][keyId]
+        
+    
     
     def subsumeGene(self, gene):
         # copy ids
@@ -76,7 +130,6 @@ class Gene(object):
                 self.addCommonName(id, gene.commonNameDict[source][id] ,source)
         for source in gene.sources:
             self.addSource(source)
-        for source in gene.synonymDict:
-            for syn in gene.synonymDict[source]:
-                self.addSynonym(syn, source)        
+        for syn in gene.synonymList:
+            self.addSynonym(syn)        
             
