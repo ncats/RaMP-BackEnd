063429695ec1271fadbff96f8c895046fb7ded86
johnbraisted <jb212828@gmail.com>
2021-06-02 17:53:30 -0400

    ontology parsing, load xml if None for Ontology

--- src/parse/hmdbData.py
+++ src/parse/hmdbData.py
@@ -583,10 +583,10 @@ class hmdbData(MetabolomicsData):
 
 
 
-    def getOntology(self, tree = None):
+    def getOntology(self, tree = None, dir = 'hmdb_metabolites.xml'):
         # get disposition
         if tree is None:
-            tree = ET.parse('../../misc/data/hmdb/' + dir)
+            tree = ET.parse('../misc/data/hmdb/' + dir)
         
         root = tree.getroot()
         
