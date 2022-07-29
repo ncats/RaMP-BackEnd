460f51a4adbab29d4af55b5988258317f6180f5b
johnbraisted <jb212828@gmail.com>
2022-07-29 16:41:05 -0400

    mods for rhea prod testing

--- src/rampEntity/RheaReaction.py
+++ src/rampEntity/RheaReaction.py
@@ -163,6 +163,11 @@ class RheaReaction(object):
                 if name is None:
                     name = "UNK"
                     print("export rxn to prot, HAVE NAME DICT, BUT NO NAME for uniprot: "+uniprot + " DICT LEN: " + str(len(list(names.keys()))))
+                else:
+                    print("Have a name in rxt to prot... but it's an empty string **|"+name+"|**")
+                    if name == "":
+                        name = "UNK5"
+                        
  
             else:
                 name = "UNK2"
